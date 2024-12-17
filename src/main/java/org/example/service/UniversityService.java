cce4d97ca3c9e28e306a2c89f29304c93de28468
Rahul <ra.raj89@gmail.com>
2024-12-16 19:38:41 -0600

    created interface for service

--- src/main/java/org/example/service/UniversityService.java
+++ src/main/java/org/example/service/UniversityService.java
@@ -1,34 +1,10 @@
-package org.example.service;
+    package org.example.service;
 
 import org.example.pojo.University;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class UniversityService {
-
-    @Autowired
-    public RestTemplate restTemplate;
-
-    @Value("${api.url}")
-    private String apiUrl;
-
-    public List<University> getUniversityListByCountry(List<String> countries){
-        List<University> allUniversities = new ArrayList<>();
-        for (String country : countries){
-            List<University> universities = restTemplate.getForObject(apiUrl + "?country=" + country, List.class);
-            if(universities != null)
-                allUniversities.addAll(universities);
-        }
-        return allUniversities;
-    }
-
-    public List<University> getUniversityList(){
-        return restTemplate.getForObject(apiUrl, List.class);
-    }
+public interface UniversityService {
+    public List<University> getUniversityList();
+    public List<University> getUniversityListByCountry(List<String> countries);
 }
